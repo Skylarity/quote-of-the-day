8575600a3672b7caf50111c8215ab5cb966ba7f3
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-28 21:11:25 -0600

    Added quotes

--- index.php
+++ index.php
@@ -1,81 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-		<!-- Bootstrap Latest compiled and minified CSS -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+        <!-- Bootstrap Latest compiled and minified CSS -->
+        <link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
 
-		<!-- Optional Bootstrap theme -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
-			  rel="stylesheet"/>
+        <!-- Optional Bootstrap theme -->
+        <link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+              rel="stylesheet"/>
 
-		<!-- Google WebFonts -->
-		<link
-			href='//fonts.googleapis.com/css?family=Roboto:400,100italic,100,300,300italic,400italic,500,500italic,700,700italic'
-			rel='stylesheet' type='text/css'>
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-		<!-- Custom CSS -->
-		<link type="text/css" href="css/style.css" rel="stylesheet"/>
+        <!-- Google WebFonts -->
+        <link
+            href='//fonts.googleapis.com/css?family=Roboto:400,100italic,100,300,300italic,400italic,500,500italic,700,700italic'
+            rel='stylesheet' type='text/css'>
 
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+        <!-- Custom CSS -->
+        <link type="text/css" href="css/style.css" rel="stylesheet"/>
 
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-		<script type="text/javascript"
-				src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript"
-				src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
-		<script type="text/javascript"
-				src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-		<title>Class Quote of the Day</title>
-	</head>
-	<body class="sfooter">
-		<div class="sfooter-content">
-			<div class="white-transparent">
-				<header>
-					<div class="container">
-						<div class="logo">
-							Quote of the Day
-						</div>
-					</div>
-				</header>
-				<div class="container">
-					<div class="row">
-						<h1>
-							"Quote of the day here"
-							<div class="attribution">
-								-Attribution
-							</div>
-						</h1>
-					</div>
-					<div class="row">
-						<div class="col-md-6">
-							<p>
-								Past quotes here!
-							</p>
-						</div>
-						<div class="col-md-6">
-							<p>
-								Past quotes here!
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<footer>
-			&copy; Created by Skyler Rexroad
-		</footer>
-	</body>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script type="text/javascript"
+                src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+        <script type="text/javascript"
+                src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+        <script type="text/javascript"
+                src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+        <!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+        <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <title>Class Quote of the Day</title>
+    </head>
+    <body class="sfooter">
+        <div class="sfooter-content">
+            <div class="white-transparent">
+                <header>
+                    <div class="container">
+                        <div class="logo">
+                            <i class="fa fa-quote-left"></i> <i class="fa fa-quote-right"></i> Quote of the Day
+                        </div>
+                    </div>
+                </header>
+                <div class="container">
+                    <div class="row">
+                        <h1>
+                            "Quote of the day here"
+                            <div class="attribution">
+                                -Attribution
+                            </div>
+                        </h1>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>
+                                Past quotes here!
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>
+                                Past quotes here!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            &copy; Created by Skyler Rexroad in 2015
+        </footer>
+    </body>
 </html>
