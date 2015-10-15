cbf1a0ca91a0a09be5402bcc77975278e3b88143
Skyler Rexroad <skyrex1095@gmail.com>
2015-10-15 09:03:33 -0600

    Basic templating

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$CURRENT_DIR = __DIR__;
+require_once("php/lib/head-utils.php");
+?>
+<div class="sfooter-content">
+	<h1>Quote of the Day</h1>
+</div>
+<?php //require_once("php/lib/footer.php"); ?>
+</body>
+</html>
