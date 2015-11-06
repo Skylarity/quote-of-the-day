e2ffa95daed8a2cc66a08250c61f54ef3b7a3b3a
Skyler Rexroad <skyrex1095@gmail.com>
2015-11-06 10:57:56 -0700

    Added form placeholder to sidebar

--- php/lib/sidebar.php
+++ php/lib/sidebar.php
@@ -1,18 +1,29 @@
-<div class="sidebar">
+<div class="sidebar bg-color-darker rounded-bottom padding">
 	<div class="row">
 		<div class="col-md-12">
-			<div class="bg-color-darker rounded-bottom padding">
-				<ul class="list-unstyled">
-					<li>A list</li>
-					<li>of past quotes</li>
-				</ul>
-				<p>
-					<code>// TODO: Make this fill height with flexbox.</code>
-				</p>
+			<ul class="list-unstyled">
+				<li>A list</li>
+				<li>of past quotes</li>
+			</ul>
+			<p>
+				<code>// TODO: Make this fill height with flexbox.</code>
+			</p>
 
-				<p>
-					<code>// Also TODO: <strong>Murder whoever thought CSS was a good idea.</strong></code>
-				</p>
+			<p>
+				<code>// Also TODO: <strong>Murder whoever thought CSS was a good idea.</strong></code>
+			</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<button class="btn bg-color-lighter" type="button" data-toggle="collapse" data-target="#addAQuote"
+					aria-expanded="false" aria-controls="addAQuote">
+				Add a quote
+			</button>
+			<div class="collapse" id="addAQuote">
+				<div class="bg-color-lighter padding">
+					Form to add a quote.
+				</div>
 			</div>
 		</div>
 	</div>
